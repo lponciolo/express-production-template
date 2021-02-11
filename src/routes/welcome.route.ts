80a99c89e20a8328b8b5ae4e0104a1cedf804a32
Leonardo <l.ponciolo@gmail.com>
2021-02-11 18:51:12 -0300

    refactor: module export & import

--- src/routes/welcome.route.ts
+++ src/routes/welcome.route.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 const router = express.Router()
-
+import { welcomeValidationSchema } from '../validators/welcome.validation'
 // import controllers
 
 import { getWelcomeController } from '../controllers/wellcome.controller'
--- src/validators/login.validation.ts
+++ src/validators/login.validation.ts
@@ -2,24 +2,25 @@ import { NextFunction, Request, Response } from 'express'
 import Joi from 'joi'
 import validateRequest from './index'
 
+export const Schema = Joi.object({
+  username: Joi.string().required().empty().messages({
+    'string.base': `"username" should be a type of 'text'`,
+    'string.empty': `"username" cannot be an empty field`,
+    'any.required': `"username" is a required field`,
+  }),
+  password: Joi.string().required().messages({
+    'string.base': `"password" should be a type of 'text'`,
+    'string.empty': `"password" cannot be an empty field`,
+    'any.required': `"password" is a required field`,
+  }),
+})
+
 const loginValidationSchema = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const schema = Joi.object({
-    username: Joi.string().required().empty().messages({
-      'string.base': `"username" should be a type of 'text'`,
-      'string.empty': `"username" cannot be an empty field`,
-      'any.required': `"username" is a required field`,
-    }),
-    password: Joi.string().required().messages({
-      'string.base': `"password" should be a type of 'text'`,
-      'string.empty': `"password" cannot be an empty field`,
-      'any.required': `"password" is a required field`,
-    }),
-  })
-  validateRequest(req, next, schema)
+  return validateRequest(req, next, Schema)
 }
 
-export default loginValidationSchema
+export { loginValidationSchema }
