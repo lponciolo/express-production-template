1fadbddaa730923ce6ecb7f6d4d2680ae105d742
Leonardo <l.ponciolo@gmail.com>
2021-02-12 22:18:10 -0300

    feat: add validators to all routes, change messages to default Joi error messages

--- src/validators/login.validation.ts
+++ src/validators/login.validation.ts
@@ -3,16 +3,8 @@ import Joi from 'joi'
 import validateRequest from './index'
 
 const Schema = Joi.object({
-  username: Joi.string().required().empty().messages({
-    'string.base': `"username" should be a type of 'text'`,
-    'string.empty': `"username" cannot be an empty field`,
-    'any.required': `"username" is a required field`,
-  }),
-  password: Joi.string().required().messages({
-    'string.base': `"password" should be a type of 'text'`,
-    'string.empty': `"password" cannot be an empty field`,
-    'any.required': `"password" is a required field`,
-  }),
+  username: Joi.string().required().empty(),
+  password: Joi.string().required(),
 })
 
 const loginValidationSchema = (
