7ad5a8135b9dedcee1231ce706f38b5cdb8c4b62
Leonardo <l.ponciolo@gmail.com>
2021-02-14 18:54:05 -0300

    should always call next function

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,22 +1,21 @@
 import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
 
 const authenticateJWT = (req: any, res: any, next: any) => {
   const accessTokenSecret = process.env.TOKEN_SECRET as string
-
   const authHeader = req.headers.authorization
 
   if (authHeader) {
     const token = authHeader.split(' ')[1]
-
     jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
       if (err) {
-        return res.sendStatus(403)
+        return next(createError(403))
       }
       req.user = user
-      next()
+      return next()
     })
   } else {
-    return res.sendStatus(401)
+    return next(createError(401))
   }
 }
 
